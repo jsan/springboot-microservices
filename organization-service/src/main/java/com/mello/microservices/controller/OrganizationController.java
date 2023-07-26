d6921e6b462fdb392228e19bc58acc9b69864393
Jorge Sansone <jorge.sansone@gmail.com>
2023-07-26 10:59:20 +0100

    organization-service implementation

---
+++ organization-service/src/main/java/com/mello/microservices/controller/OrganizationController.java
@@ -0,0 +1,31 @@
+package com.mello.microservices.controller;
+
+import com.mello.microservices.dto.OrganizationDto;
+import lombok.AllArgsConstructor;
+import com.mello.microservices.service.OrganizationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("organizations")
+@AllArgsConstructor
+public class OrganizationController {
+
+    private OrganizationService organizationService;
+
+    // Build Save Organization REST API
+    @PostMapping
+    public ResponseEntity<OrganizationDto> saveOrganization(@RequestBody OrganizationDto organizationDto){
+        OrganizationDto savedOrganization = organizationService.saveOrganization(organizationDto);
+        return new ResponseEntity<>(savedOrganization, HttpStatus.CREATED);
+    }
+
+    // Build Get Organization by Code REST API
+    @GetMapping("{code}")
+    public ResponseEntity<OrganizationDto> getOrganization(@PathVariable("code") String organizationCode){
+        OrganizationDto organizationDto = organizationService.getOrganizationByCode(organizationCode);
+        return ResponseEntity.ok(organizationDto);
+    }
+
+}
